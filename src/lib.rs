757342eba56eb83a8688a6384ea9d3a08118a958
Erik Hollensbe <git@hollensbe.org>
2023-09-02 15:26:14 -0700

    more dox
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! gcal: Another Google Calendar API library for rust-lang
+//!
+//! I wrote this by hand because I found other clients hard to use for my use-cases. This provides an API layer into the Google Calendar API that is very minimal but also mostly complete. Types are fully represented.
+//!
+//! ## Example
+//!
+//! ```ignore
+//! use gcal::*;
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), anyhow::Error> {
+//!     let access_key = std::env::args().nth(1).expect("Provide an access key");
+//!     let now = chrono::Local::now();
+//!     let client = Client::new(access_key);
+//!     let client = EventClient::new(client);
+//!     let list = client.list(now - chrono::Duration::days(1), now).await?;
+//!
+//!     for event in &list {
+//!         eprintln!("{} {}", event.id, event.summary);
+//!     }
+//! }
+//! ```
 pub mod client;
 pub mod resources;
 pub mod sendable;
